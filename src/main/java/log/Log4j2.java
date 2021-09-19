a010f71ab86e4b18baf1be29918aea66e7eee6b9
Juan Debenedetti <juandebenedetti94@gmail.com>
2021-09-19 06:16:59 -0300

    Update

--- src/main/java/log/Log4j2.java
+++ src/main/java/log/Log4j2.java
@@ -2,15 +2,18 @@ package log;
 
 import org.apache.logging.log4j.Logger;
 
-import _LABORATORIO.Prueba;
-
 import org.apache.logging.log4j.LogManager;
 
 /**
  * Log4j 2 API "Hello world": https://logging.apache.org/log4j/2.x/manual/api.html
+ * Arquitectura: https://logging.apache.org/log4j/2.x/manual/architecture.html
  * Configuracion: https://logging.apache.org/log4j/2.x/manual/configuration.html
+ * Appenders: https://logging.apache.org/log4j/2.x/manual/appenders.html
+ * Layouts: https://logging.apache.org/log4j/2.x/manual/layouts.html
  * API: https://logging.apache.org/log4j/2.x/log4j-api/apidocs/index.html
  * Tutorial: https://mkyong.com/logging/apache-log4j-2-tutorials/
+ * 
+ * En el archivo config xml no se respeta el nivel del logger para la consola, pero para el logger de file si.
  * 
  * @author Ru$o
  * 
@@ -22,7 +25,9 @@ public class Log4j2 {
 	 * LogManager.getLogger(...). Debido a que este uso es tan comun, Log4j 2 lo proporciona como predeterminado cuando el
 	 * parametro del nombre del registrador se omite o es nulo.
 	 * Log4j2.class o Log4j2.class.getName() son dos formas diferentes de pasarle el nombre al logger, pero para una mejor
-	 * legibilidad se omite. */
+	 * legibilidad se omite.
+	 * El registrador en si no realiza acciones directas. Simplemente tiene un nombre y esta asociado con un
+	 * LoggerConfig. */
 	private static final Logger log = LogManager.getLogger();
 
 	public static void main(String[] args) {
@@ -35,8 +40,6 @@ public class Log4j2 {
 		 * Ahora con Java 8 puede lograr el mismo efecto con una expresion lambda. Ya no es necesario verificar explicitamente
 		 * el nivel de registro: */
 		log.debug("{}", () -> getNumber());
-
-		new Prueba();
 
 	}
 
