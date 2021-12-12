719a9e2e6de7d55dcf8225ed85394933fc1f37ec
Juan Debenedetti <juandebenedetti94@gmail.com>
2021-12-11 21:29:48 -0300

    update

--- src/main/java/_LABORATORIO/Prueba.java
+++ src/main/java/_LABORATORIO/Prueba.java
@@ -2,4 +2,21 @@ package _LABORATORIO;
 
 public class Prueba {
 
+	public static void main(String[] args) {
+
+		long startTime = System.nanoTime();
+
+		int a = 0, b = 1, c = 1, d = 0, e = 1, f = 1;
+
+		a = b + c;
+		d = e + f;
+
+		System.out.println(a);
+		System.out.println(d);
+
+		long endTime = System.nanoTime();
+		System.out.println("\nDuracion: " + (endTime - startTime) / 1e6 + " ms");
+
+	}
+
 }
