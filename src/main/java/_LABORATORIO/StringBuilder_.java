3cf8b2b000e43c599d146529d9034c8acf892af2
Juan Debenedetti <juandebenedetti94@gmail.com>
2022-08-02 20:04:57 -0300

    Update

--- src/main/java/_LABORATORIO/StringBuilder_.java
+++ src/main/java/_LABORATORIO/StringBuilder_.java
@@ -1,22 +1,21 @@
 package _LABORATORIO;
 
 /**
- * -STRING_BUILDER
- * .Es mutable.
- * .Se almacena en el heap, ya que es un objeto.
- * .Se crea con una capacidad inicial de 16 caracteres por defecto.
- * .No es thread-Safe (problemas de interferencia)
- * .Es mucho mas rapida que StringBuffer.
- * 
- * �Cuando usarla?
- * Si la cadena va a ser modificada desde un hilo.
- * 
+ * Una secuencia mutable de caracteres. Esta clase proporciona una API compatible con StringBuffer, pero sin garantia de
+ * sincronizacion. Esta clase esta diseñada para usarse como un reemplazo directo para StringBuffer en lugares donde el
+ * buffer de cadena estaba siendo utilizado por un solo subproceso (como suele ser el caso). Siempre que sea posible, se
+ * recomienda utilizar esta clase con preferencia a StringBuffer, ya que sera mas rapida en la mayoria de las
+ * implementaciones.
+ * <ul>
+ * <li>Es mutable.
+ * <li>Se almacena en el heap, ya que es un objeto.
+ * <li>Se crea con una capacidad inicial de 16 caracteres por defecto.
+ * <li>No es thread-Safe (problemas de interferencia)
+ * <li>Es mucho mas rapida que StringBuffer.
+ * </ul>
+ * ¿Cuando usarla? Si la cadena va a ser modificada desde un hilo.
  */
 
 public class StringBuilder_ {
-
-	public static void main(String[] args) {
-
-	}
 
 }
