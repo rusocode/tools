ef451b9d44627ccca7641d3f5a889a5b0ca6a19f
Juan Debenedetti <juandebenedetti94@gmail.com>
2022-08-29 12:30:32 -0300

    Update

--- src/main/java/game_development/CpuClock.java
+++ src/main/java/game_development/CpuClock.java
@@ -25,7 +25,21 @@ package game_development;
  * de doble nucleo de 3,0 GHz seria capaz de realizar el doble de calculos que un procesador de un solo nucleo de 3,10
  * GHz.
  *
+ * <br><br>
+ *
+ * <h3>Diferencia entre el reloj del sistema y el reloj del cpu</h3>
+ * El reloj del sistema es necesario para sincronizar todos los componentes de la placa base, lo que significa que
+ * todos hacen su trabajo solo si el reloj esta alto; nunca cuando esta bajo. Y debido a que la velocidad del reloj se
+ * establece por encima del tiempo mas largo que cualquier señal necesita para propagarse a traves de cualquier
+ * circuito en la placa, este sistema evita que las señales lleguen antes de que otras señales esten listas y, por lo
+ * tanto, mantiene todo seguro y sincronizado. El reloj de la CPU tiene el mismo proposito, pero solo se usa en el
+ * propio chip. Debido a que la CPU necesita realizar mas operaciones por vez que la placa base, el reloj de la CPU es
+ * mucho mas alto. Y como no queremos tener otro oscilador (por ejemplo, porque tambien tendrian que estar
+ * sincronizados), la CPU simplemente toma el reloj del sistema y lo multiplica por un numero, que esta fijo o
+ * desbloqueado (en ese caso, el usuario puede cambiar el multiplicador para hacer overclocking o underclockear la CPU).
+ *
  * <p>Recursos:
+ * <a href="https://stackoverflow.com/questions/43651954/what-is-a-clock-cycle-and-clock-speed">¿Que es un ciclo de reloj y la velocidad del reloj?</a>
  * <a href="https://www.computerhope.com/jargon/c/clockspe.htm">Velocidad del reloj</a>
  * <a href="https://www.intel.es/content/www/es/es/gaming/resources/cpu-clock-speed.html#:~:text=La%20velocidad%20de%20reloj%20mide,la%20velocidad%20de%20la%20CPU">¿Que es la velocidad de reloj de la CPU?</a>
  * <a href="https://www.youtube.com/watch?v=Bmgga8ZUiL8">¿Que es la frecuencia de un CPU?</a>
@@ -33,4 +47,25 @@ package game_development;
  */
 
 public class CpuClock {
+
+	/**
+	 * Devuelve el numero de procesadores disponibles para la maquina virtual Java en el momento en que se llama al
+	 * metodo, por lo que no necesariamente devolvera la cantidad de procesadores reales en un sistema.
+	 *
+	 * <p>Este valor puede cambiar durante una invocacion particular de la maquina virtual. Por lo tanto, las
+	 * aplicaciones que son sensibles a la cantidad de procesadores disponibles deben sondear ocasionalmente esta
+	 * propiedad y ajustar su uso de recursos de manera adecuada.
+	 *
+	 * <p>Para obtener los cantidad de procesadores reales del sistema, lo mas probable es que haya que realizar
+	 * llamadas al sistema operativo utilizando llamadas nativas a traves de Java Native Interface (JNI).
+	 *
+	 * @return el numero maximo de procesadores disponibles para la maquina virtual; nunca mas pequeño que uno.
+	 */
+	private static int avaliableProcessors() {
+		return Runtime.getRuntime().availableProcessors();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(avaliableProcessors());
+	}
 }
--- src/main/java/game_development/CpuTime.java
+++ src/main/java/game_development/CpuTime.java
@@ -17,44 +17,13 @@ package game_development;
  * tiempo real incluye el tiempo de E/S, los retrasos multitarea y todos los demas tipos de espera en los que incurre el
  * programa.
  *
- * <br><br>
- *
- * <h3>Diferencia entre el tiempo de sistema y el tiempo de CPU</h3>
- * El reloj del sistema es necesario para sincronizar todos los componentes de la placa base, lo que significa que
- * todos hacen su trabajo solo si el reloj esta alto; nunca cuando esta bajo. Y debido a que la velocidad del reloj se
- * establece por encima del tiempo mas largo que cualquier señal necesita para propagarse a traves de cualquier
- * circuito en la placa, este sistema evita que las señales lleguen antes de que otras señales esten listas y, por lo
- * tanto, mantiene todo seguro y sincronizado. El reloj de la CPU tiene el mismo proposito, pero solo se usa en el
- * propio chip. Debido a que la CPU necesita realizar mas operaciones por vez que la placa base, el reloj de la CPU es
- * mucho mas alto. Y como no queremos tener otro oscilador (por ejemplo, porque tambien tendrian que estar
- * sincronizados), la CPU simplemente toma el reloj del sistema y lo multiplica por un numero, que esta fijo o
- * desbloqueado (en ese caso, el usuario puede cambiar el multiplicador para hacer overclocking o underclockear la CPU).
- *
  * <p>Recursos:
  * <a href="https://stackoverflow.com/questions/7467245/cpu-execution-time-in-java">Tiempo de ejecucion de la CPU en Java</a>
+ * <a href="https://serverfault.com/questions/48455/what-are-the-differences-between-wall-clock-time-user-time-and-cpu-time">¿Cuales son las diferencias entre la hora del reloj de pared, la hora del usuario y la cpu time?</a>
  */
 
 public class CpuTime {
 
-	/**
-	 * Devuelve el numero de procesadores disponibles para la maquina virtual Java en el momento en que se llama al
-	 * metodo, por lo que no necesariamente devolvera la cantidad de procesadores reales en un sistema.
-	 *
-	 * <p>Este valor puede cambiar durante una invocacion particular de la maquina virtual. Por lo tanto, las
-	 * aplicaciones que son sensibles a la cantidad de procesadores disponibles deben sondear ocasionalmente esta
-	 * propiedad y ajustar su uso de recursos de manera adecuada.
-	 *
-	 * <p>Para obtener los cantidad de procesadores reales del sistema, lo mas probable es que haya que realizar
-	 * llamadas al sistema operativo utilizando llamadas nativas a traves de Java Native Interface (JNI).
-	 *
-	 * @return el numero maximo de procesadores disponibles para la maquina virtual; nunca mas pequeño que uno.
-	 */
-	private static int avaliableProcessors() {
-		return Runtime.getRuntime().availableProcessors();
-	}
-
-	public static void main(String[] args) {
-		System.out.println(avaliableProcessors());
-	}
+	// https://cs.stackexchange.com/questions/32149/what-are-system-clock-and-cpu-clock-and-what-are-their-functions
 
 }
