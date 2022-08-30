fd056c04f52ecbc092ec1479c305fe33d95827ac
Juan Debenedetti <juandebenedetti94@gmail.com>
2022-08-30 12:24:12 -0300

    Update

--- src/main/java/game_development/CpuClock.java
+++ src/main/java/game_development/CpuClock.java
@@ -2,33 +2,26 @@ package game_development;
 
 /**
  * <h2>Reloj del CPU</h2>
- * Denominado alternativamente como <b>velocidad del reloj</b>, <b>velocidad del procesador</b> y <b>frecuencia del reloj</b>,
- * la velocidad del reloj es la velocidad a la que el microprocesador ejecuta cada instruccion o cada vibracion del <a href="https://www.computerhope.com/jargon/c/clock.htm">reloj</a>.
- * La CPU requiere un numero fijo de ticks de reloj, o ciclos, para <a href="https://www.computerhope.com/jargon/e/execute.htm">ejecutar</a>
- * cada <a href="https://www.computerhope.com/jargon/c/compinst.htm">instruccion</a>. Tu CPU procesa muchas
- * instrucciones (calculos de bajo nivel como los aritmeticos) a partir de diferentes programas a cada segundo.
+ * En pocas palabras, el reloj del procesador (tambien conocido como «frecuencia de reloj» o «frecuencia») es la
+ * velocidad a la que el procesador (circuito electrico) ejecuta cada instruccion por segundo.
  *
- * <p>Un "ciclo" es tecnicamente un pulso sincronizado por un oscilador interno, pero, para nuestros fines, es una
- * unidad basica que ayuda a comprender la velocidad de una CPU. Durante cada ciclo, miles de millones de transistores
- * dentro del procesador se abren y cierran.
+ * <p>Los procesadores funcionan por ciclos. Por ejemplo, a una calculadora que tiene una lista de operaciones para
+ * calcular y devolver el resultado se le llama ciclo.
  *
- * <p>Cuanto mayor sea la <a href="https://www.computerhope.com/jargon/f/frequenc.htm">frecuencia</a> del reloj de la
- * CPU, mas operaciones logicas puede realizar por segundo. Entonces, a medida que aumenta la frecuencia del reloj de la
- * CPU, disminuye el tiempo requerido para realizar las tareas.
+ * <p>La frecuencia (ciclos por segundo) se regula desde un oscilador de cristal. Este es un componente electronico que
+ * esta echo de cristal, normalmente de cuarzo y tiene la capacidad de mandar al procesador a funcionar de forma
+ * periodica. Este componente se conoce como <i>reloj</i>. Hay varios relojes de este tipo en tu computadora (reloj de
+ * pared, reloj de sistema, y algunos otros), pero vamos a obviar todo eso y centrarnos en el unico que nos interesa
+ * ahora mismo, y que controla la frecuencia de los nucleos de tu procesador.
  *
- * <p>La velocidad del reloj se miden en Hertz, 1 MHz representa 1 millon de ciclos por segundo, o en GHz, 1 GHz
- * representa 1 mil millones de ciclos por segundo. Cuanto mayor sea la velocidad de la CPU, mejor sera el rendimiento
- * de una computadora, en un sentido general. Otros componentes como la memoria RAM, el disco duro, la placa base y la
- * cantidad de nucleos del procesador (por ejemplo, de dos o cuatro nucleos) tambien pueden mejorar la velocidad de la
- * computadora.
+ * <p>La velocidad del reloj se miden en Hertz. Cuanto mayor sea la frecuencia del reloj, mas operaciones puede realizar
+ * por segundo. Otros componentes como la memoria RAM, el disco duro, la placa base y la cantidad de nucleos del
+ * procesador (por ejemplo, de dos o cuatro nucleos) pueden mejorar la velocidad de la computadora.
  *
- * <p>La velocidad de la CPU determina cuantos calculos puede realizar en un segundo de tiempo. Cuanto mayor sea la
- * velocidad, mas calculos puede realizar, lo que hace que la computadora sea mas rapida. Si bien hay varias marcas de
- * procesadores de computadora disponibles, incluidas Intel y AMD, todas usan el mismo estandar de velocidad de CPU para
- * determinar que velocidad ejecuta cada uno de sus procesadores. Si un procesador tiene dos o cuatro nucleos, el
- * rendimiento de la computadora puede aumentar incluso si la velocidad de la CPU sigue siendo la misma. Un procesador
- * de doble nucleo de 3,0 GHz seria capaz de realizar el doble de calculos que un procesador de un solo nucleo de 3,10
- * GHz.
+ * <p>Normalmente el reloj tiene por defecto una frecuencia base de 1 GHz. Eso significa que de base, el reloj manda
+ * al procesador a hacer un millon de operaciones (ciclos) por segundo. Cada ciclo del procesador es un conjunto de
+ * operaciones con un resultado. En su version mas basica podemos imaginar que es una suma, una resta, etc. Todas estas
+ * operaciones son las que hacen funcionar a la computadora.
  *
  * <br><br>
  *
@@ -44,11 +37,14 @@ package game_development;
  * desbloqueado (en ese caso, el usuario puede cambiar el multiplicador para hacer overclocking o underclockear la CPU).
  *
  * <p>Recursos:
+ * <a href="https://www.youtube.com/watch?v=SeNrVQBRHqE">¡Antes de hacer Overclock, mira esto!</a>
  * <a href="https://www.intel.es/content/www/es/es/gaming/resources/cpu-clock-speed.html#:~:text=La%20velocidad%20de%20reloj%20mide,la%20velocidad%20de%20la%20CPU">¿Que es la velocidad de reloj?</a>
  * <a href="https://www.computerhope.com/jargon/c/clockspe.htm">Velocidad de reloj</a>
  * <a href="https://www.youtube.com/watch?v=Bmgga8ZUiL8">¿Que es la frecuencia de un CPU?</a>
  * <a href="https://cs.stackexchange.com/questions/32149/what-are-system-clock-and-cpu-clock-and-what-are-their-functions">Diferencia entre el reloj del sistema y el reloj del cpu</a>
  * <a href="https://stackoverflow.com/questions/43651954/what-is-a-clock-cycle-and-clock-speed">¿Que es un ciclo de reloj y la velocidad del reloj?</a>
+ *
+ * @author Juan
  */
 
 public class CpuClock {
