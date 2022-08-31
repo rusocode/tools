fea615777caef1dd3c0b16c9675e7a93f38c3ef4
Juan Debenedetti <juandebenedetti94@gmail.com>
2022-08-30 22:42:27 -0300

    Update

--- src/main/java/gamedev/GameLoop.java
+++ src/main/java/gamedev/GameLoop.java
@@ -2,8 +2,8 @@ package gamedev;
 
 /**
  * <p>Recursos:
- * <a href="https://gamedev.stackexchange.com/questions/1589/when-should-i-use-a-fixed-or-variable-time-step">¿Cuando debo usar un timestep fijo o variable?</a>
  * <a href="http://gameprogrammingpatterns.com/game-loop.html">Game Loop</a>
+ * <a href="https://gamedev.stackexchange.com/questions/1589/when-should-i-use-a-fixed-or-variable-time-step">¿Cuando debo usar un timestep fijo o variable?</a>
  * <a href="https://gamedev.stackexchange.com/questions/160329/java-game-loop-efficiency">Eficiencia del Game Loop</a>
  * <a href="https://gamedev.stackexchange.com/questions/132831/what-is-the-point-of-update-independent-rendering-in-a-game-loop">¿Cual es el punto de actualizar (tick) independientemente del render en el Game Loop?</a>
  * <a href="https://gamedev.stackexchange.com/questions/81608/what-is-a-tick-in-the-context-of-game-development#:~:text=A%20tick%20is%20a%20unit,%22%20and%20%22tock%22">¿Que es un "tick" en el contexto del desarrollo de juegos?</a>
