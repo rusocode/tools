81f31e3773b5eb287ef0faef68cdab99566b9cff
Juan Debenedetti <juandebenedetti94@gmail.com>
2023-02-08 17:14:38 -0300

    Update

--- src/main/java/_lab/Test.java
+++ src/main/java/_lab/Test.java
@@ -2,15 +2,4 @@ package _lab;
 
 public class Test {
 
-	public String publicVar = "rulo quemado";
-	private final String privateVar = "mi valor es privado";
-
-	public void publicMethod() {
-
-	}
-
-	private void privateMethod() {
-
-	}
-
 }
--- src/main/java/events/teclado/_1.java
+++ src/main/java/events/teclado/_1.java
@@ -10,63 +10,57 @@ import javax.swing.JFrame;
 
 /**
  * ¿Que sucede realmente con los eventos?
- * 
+ * <p>
  * Pulsacion de tecla = evento (suceso)
  * Nosotros consideramos a este suceso como algo intangible, pero java lo considera como un OBJETO.
  * Java lo que hace es empaquetar al suceso en un objeto (suceso = objeto).
- * 
+ * <p>
  * A ese paquete se le añaden una serie de caracteristicas, por ejemplo en el caso de una tecla pulsada, el suceso (objeto) seria:
  * tipo de objeto: KeyEvent
  * tipo de evento: Pressed
- * 
+ * <p>
  * Lo que ocurre despues de empaquetar ese evento, es que viaja y se almacena en el parametro del metodo keyPressed(KeyEvent objeto)
  * del objeto Oyente (KeyListener).
- * 
  */
 
 public class _1 {
 
 	public static void main(String[] args) {
-
 		new Marco().setVisible(true);
-
 	}
 }
 
 class Marco extends JFrame {
 
+
 	public Marco() {
-
-		// Agrega el Oyente de tipo KeyListener a la ventana.
-		addKeyListener(new Oyente());
-
 		setTitle("Ventana");
-		setSize(Toolkit.getDefaultToolkit().getScreenSize().width / 2, Toolkit.getDefaultToolkit()
-				.getScreenSize().height / 2);
+		setSize(Toolkit.getDefaultToolkit().getScreenSize().width / 2, Toolkit.getDefaultToolkit().getScreenSize().height / 2);
 		setLocationRelativeTo(null);
-		setDefaultCloseOperation(3);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addKeyListener(new Oyente());
 	}
 
-	private class Oyente implements KeyListener {
+	private static class Oyente implements KeyListener {
 
-		// Tecla presionada (_).
-		public void keyPressed(KeyEvent e ) { /* e PASA A SER DE UN SUCESO A UN OBJETO!!!!!!!!!!!*/
-			// Imprime el codigo de la tecla pulsada.
-			// System.out.println(e.getKeyCode());
-			System.out.println("Pulsada");
+		private int lastKey = -1;
 
+		public void keyPressed(KeyEvent e) {
+			// Si la ultima tecla presionada es distinta a la actual, entonces ejecuta la accion
+			if (lastKey != e.getKeyCode()) {
+				System.out.println("Tecla presionada");
+				lastKey = e.getKeyCode();
+			}
 		}
 
-		// Tecla soltada (-).
 		public void keyReleased(KeyEvent e) {
-			System.out.println("Soltada");
-
+			lastKey = -1; // Asigna de nuevo -1 para que se pueda volver a condicionar como una tecla distinta a la actual
+			System.out.println("Tecla soltada");
 		}
 
-		// Tecla pulsada y soltada (-), combinacion de tecla pulsada y soltada.
+		// Combinacion de tecla pulsada y soltada
 		public void keyTyped(KeyEvent e) {
-			// System.out.println(e.getKeyChar());
-			System.out.println("Se toco la tecla: " + e.getKeyChar());
+			// System.out.println("Pulsada y soltada");
 		}
 
 	}
