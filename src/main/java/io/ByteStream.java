eec34032d34d44364e8126c9568a01544f3ce740
Juan Debenedetti <juandebenedetti94@gmail.com>
2023-06-27 18:34:09 -0300

    Update

--- src/main/java/io/ByteStream.java
+++ src/main/java/io/ByteStream.java
@@ -6,7 +6,7 @@ import static utils.Constants.*;
 
 /**
  * Las clases FileInputStream y FileOutputStream, hacen posible leer y escribir un archivo como un flujo de bytes.
- * 
+ *
  * <p>
  * <b>Codificacion Unicode en UTF-8</b><br>
  * Supongamos que tenemos una hoja de texto con un mensaje codificado en bianario, entonces si nosotros creamos un flujo
@@ -18,7 +18,7 @@ import static utils.Constants.*;
  * a 255, que es la representacion de 1 bytes. En el ejemplo anterior usamos el code point U+0054 que seria 54 en en
  * hexadecimal, 84 en decimal y finalmente 01010100 en binario, haciendo posible su envio a travez de la red. Esta
  * transformacion en binario la hace el formato UTF-8 para que la maquina lo pueda entender.
- * 
+ * <p>
  * ASCII (ANSI X3.4) es el codigo estandar estadounidense para el intercambio de informacion. Los primeros 128
  * caracteres de la codificacion Unicode UTF-16 son los caracteres ASCII.
  * El sitema ASCII representa 128 caracteres, los decimales entre 0 y 127, ambos incluidos, que serian los code
@@ -27,12 +27,10 @@ import static utils.Constants.*;
  * positivo, es decir que el primer bit representa el signo del numero. Entonces se podria decir que 1 byte en java
  * representa los numeros entre -128 y 127, ambos incluidos, sumando un total de 256 caracteres (el 0 tambien se suma).
  * A esto se le llama complemento a dos.
- * 
+ * <p>
  * https://stackoverflow.com/questions/3621067/why-is-the-range-of-bytes-128-to-127-in-java#:~:text=8%20Answers&text=The%20answer%20is%20two's%20complement,a%207-bit%20unsigned%20integer.
  * https://en.wikipedia.org/wiki/Two%27s_complement
  * https://www.youtube.com/watch?v=M_yNoV3c8DY
- * </p>
- * 
  * <p>
  * <b>Rendimiento de lectura usando buffers</b><br>
  * Teniendo una base del funcionamiento de Unicode, podriamos decir que el tubo (flujo) almacena el code point en un
@@ -48,217 +46,215 @@ import static utils.Constants.*;
  * obtendra una mayor aceleracion de un array de bytes mas grande.
  * Probablemente tendra que experimentar con diferentes tamaños de array de bytes y medir el rendimiento de lectura
  * para encontrar el tamaño de array de bytes optimo.
- * </p>
- * 
  * <p>
  * <b>Rendimiento de escritura usando buffers</b><br>
  * Es mas rapido escribir un array de bytes en un FileOutputStream que escribir un byte a la vez. La
  * aceleracion puede ser bastante significativa, hasta 10 veces mayor o mas. Por lo tanto, se recomienda utilizar los
  * metodos de escritura a travez de un array siempre que sea posible.
- * 
+ * <p>
  * La aceleracion exacta que obtiene depende del sistema operativo subyacente y el hardware de la computadora en la que
  * ejecuta el codigo Java. La aceleracion depende de cuestiones como la velocidad de la memoria, la velocidad del disco
  * duro y el tamaño del buffer.
- * </p>
- * 
- * @author Juan Debenedetti aka Ru$o
- * 
+ *
+ * @author Juan Debenedetti
  */
 
 public class ByteStream {
 
-	private File file;
-	private FileInputStream input;
-	private FileOutputStream output;
+    private File file;
+    private FileInputStream input;
+    private FileOutputStream output;
 
-	/** Crea un flujo para el archivo usando un objeto File. */
-	public ByteStream(File file) {
-		this.file = file;
-	}
+    /**
+     * Crea un flujo para el archivo usando un objeto File.
+     */
+    public ByteStream(File file) {
+        this.file = file;
+    }
 
-	/**
-	 * Crea un flujo de entrada para el archivo de texto y hace llamadas nativas, en donde decodifica el "code point"
-	 * utilizando el formato predetermiando de la plataforma.
-	 * Este metodo se bloquea si aun no hay ninguna entrada disponible.
-	 */
-	private void readText() {
+    /**
+     * Crea un flujo de entrada para el archivo de texto y hace llamadas nativas, en donde decodifica el "code point"
+     * utilizando el formato predetermiando de la plataforma.
+     * Este metodo se bloquea si aun no hay ninguna entrada disponible.
+     */
+    private void readText() {
 
-		int codepoint, i = 0;
+        int codepoint, i = 0;
 
-		char[] chars = null; // Array para almacenar caracteres
-		byte[] Int8 = null; // Array para alamcenar decimales con signo de -128 a 127
-		int[] UInt8 = null; // Array para almacenar decimales sin singo de 0 a 255
+        char[] chars = null; // Array para almacenar caracteres
+        byte[] Int8 = null; // Array para alamcenar decimales con signo de -128 a 127
+        int[] UInt8 = null; // Array para almacenar decimales sin singo de 0 a 255
 
-		try {
+        try {
 
-			input = new FileInputStream(file);
+            input = new FileInputStream(file);
 
-			/* Inicializa los arrays de diferentes formas, tomando el tamaño total del archivo.
-			 * 
-			 * FIXME ¿Esto resulta eficiente en terminos de rendimiento? Ya que no se estarian creando espacios sobrantes como en el
-			 * caso de un array convencional de 8192 posiciones? */
-			chars = new char[(int) input.getChannel().size()];
-			Int8 = new byte[(int) file.length()];
-			UInt8 = new int[input.available()];
+            /* Inicializa los arrays de diferentes formas, tomando el tamaño total del archivo.
+             *
+             * FIXME ¿Esto resulta eficiente en terminos de rendimiento? Ya que no se estarian creando espacios sobrantes como en el
+             * caso de un array convencional de 8192 posiciones? */
+            chars = new char[(int) input.getChannel().size()];
+            Int8 = new byte[(int) file.length()];
+            UInt8 = new int[input.available()];
 
-			System.out.println("Archivo: " + file.getName());
-			System.out.println("Ruta: " + file.getPath());
-			System.out.println("Tamaño: " + input.available() + " bytes");
+            System.out.println("Archivo: " + file.getName());
+            System.out.println("Ruta: " + file.getPath());
+            System.out.println("Tamaño: " + input.available() + " bytes");
 
-			/* Lee el archivo de texto byte por byte, en donde se devuelve un decimal entre 0 y 255 ambos incluidos en cada llamada
-			 * nativa al sistema operativo, resultado bastante ineficiente para lecturas de archivos grandes, aunque en este caso se
-			 * usan las llamadas nativas como un ejemplo de su funcionamiento.
-			 * El -1 indica el final del archivo, es decir, -1 como int, no como byte, por lo tanto, es necesario inspeccionar el
-			 * valor devuelto por estas llamadas al metodo read() por medio de un bucle while. */
-			while ((codepoint = input.read()) != -1) {
-				chars[i] = (char) codepoint; // Convierte el decimal a caracter
-				Int8[i] = (byte) codepoint; // Convierte el decimal a byte con signo
-				UInt8[i] = codepoint; // Decimales sin firmar entre 0 y 255 (U = unsigned)
-				i++;
-			}
+            /* Lee el archivo de texto byte por byte, en donde se devuelve un decimal entre 0 y 255 ambos incluidos en cada llamada
+             * nativa al sistema operativo, resultado bastante ineficiente para lecturas de archivos grandes, aunque en este caso se
+             * usan las llamadas nativas como un ejemplo de su funcionamiento.
+             * El -1 indica el final del archivo, es decir, -1 como int, no como byte, por lo tanto, es necesario inspeccionar el
+             * valor devuelto por estas llamadas al metodo read() por medio de un bucle while. */
+            while ((codepoint = input.read()) != -1) {
+                chars[i] = (char) codepoint; // Convierte el decimal a caracter
+                Int8[i] = (byte) codepoint; // Convierte el decimal a byte con signo
+                UInt8[i] = codepoint; // Decimales sin firmar entre 0 y 255 (U = unsigned)
+                i++;
+            }
 
-			System.out.println("Decodificando...");
+            System.out.println("Decodificando...");
 
-			for (i = 0; i < Int8.length; i++)
-				System.out.println("bloque " + i + " > caracter = " + chars[i] + ", byte = " + Int8[i] + ", Ubyte = " + UInt8[i]);
+            for (i = 0; i < Int8.length; i++)
+                System.out.println("bloque " + i + " > caracter = " + chars[i] + ", byte = " + Int8[i] + ", Ubyte = " + UInt8[i]);
 
-		} catch (FileNotFoundException e) {
-			System.err.println("El archivo no existe!\nMas informacion...");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.err.println("Error de I/O!\nMas informacion...");
-			e.printStackTrace();
-		} finally {
-			try {
-				if (input != null) input.close();
-			} catch (IOException e) {
-				System.err.println("No se pudo cerrar el flujo de entrada!\nMas informacion...");
-				e.printStackTrace();
-			}
-		}
+        } catch (FileNotFoundException e) {
+            System.err.println("El archivo no existe!\nMas informacion...");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Error de I/O!\nMas informacion...");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (input != null) input.close();
+            } catch (IOException e) {
+                System.err.println("No se pudo cerrar el flujo de entrada!\nMas informacion...");
+                e.printStackTrace();
+            }
+        }
 
-	}
+    }
 
-	/**
-	 * Crea un flujo de salida hacia el archivo de texto y escribe el array de bytes del mensaje.
-	 * 
-	 * @param mensaje - El mensaje que se va a escribir.
-	 * @param append  - Si es verdadero, los datos se escribiran al final del archivo en lugar de sobreescribirlos.
-	 */
-	private void writeText(String mensaje, boolean append) {
+    /**
+     * Crea un flujo de salida hacia el archivo de texto y escribe el array de bytes del mensaje.
+     *
+     * @param mensaje - El mensaje que se va a escribir.
+     * @param append  - Si es verdadero, los datos se escribiran al final del archivo en lugar de sobreescribirlos.
+     */
+    private void writeText(String mensaje, boolean append) {
 
-		/* Codifica el mensaje en una secuencia de bytes usando el juego de caracteres predeterminado de la plataforma,
-		 * almacenando el resultado en un nuevo array de bytes. */
-		byte[] bytes = mensaje.getBytes();
+        /* Codifica el mensaje en una secuencia de bytes usando el juego de caracteres predeterminado de la plataforma,
+         * almacenando el resultado en un nuevo array de bytes. */
+        byte[] bytes = mensaje.getBytes();
 
-		try {
+        try {
 
-			output = new FileOutputStream(file, append);
+            output = new FileOutputStream(file, append);
 
-			output.write(bytes);
+            output.write(bytes);
 
-		} catch (FileNotFoundException e) {
-			System.err.println("El archivo no existe!\nMas informacion...");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.err.println("Error de I/O!\nMas informacion...");
-			e.printStackTrace();
-		} finally {
-			try {
-				if (output != null) output.close();
-			} catch (IOException e) {
-				System.err.println("No se pudo cerrar el flujo de salida!\nMas informacion...");
-				e.printStackTrace();
-			}
-		}
+        } catch (FileNotFoundException e) {
+            System.err.println("El archivo no existe!\nMas informacion...");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Error de I/O!\nMas informacion...");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (output != null) output.close();
+            } catch (IOException e) {
+                System.err.println("No se pudo cerrar el flujo de salida!\nMas informacion...");
+                e.printStackTrace();
+            }
+        }
 
-	}
+    }
 
-	/**
-	 * Crea un flujo de entrada para el archivo de imagen y lee desde el buffer.
-	 * 
-	 * Es importante aclarar que el metodo read del flujo de entrada, recibe un array de bytes (buffer), en donde es llenado
-	 * por los bytes del archivo. La lectura la hacemos nosotros mismo a travez del metodo for.
-	 */
-	private void readTexture() {
+    /**
+     * Crea un flujo de entrada para el archivo de imagen y lee desde el buffer.
+     * <p>
+     * Es importante aclarar que el metodo read del flujo de entrada, recibe un array de bytes (buffer), en donde es llenado
+     * por los bytes del archivo. La lectura la hacemos nosotros mismo a travez del metodo for.
+     */
+    private void readTexture() {
 
-		byte[] buffer = null;
+        byte[] buffer = null;
 
-		try {
+        try {
 
-			input = new FileInputStream(file);
+            input = new FileInputStream(file);
 
-			buffer = new byte[input.available()];
+            buffer = new byte[input.available()];
 
-			System.out.println("Archivo: " + file.getName());
-			System.out.println("Ruta: " + file.getPath());
-			System.out.println("Tamaño: " + input.available() + " bytes");
+            System.out.println("Archivo: " + file.getName());
+            System.out.println("Ruta: " + file.getPath());
+            System.out.println("Tamaño: " + input.available() + " bytes");
 
-			long startTime = System.nanoTime();
-			while (input.read(buffer) != -1) {
-				for (int i = 0; i < buffer.length; i++)
-					System.out.println("bloque " + i + " > byte = " + buffer[i]);
-			}
-			long endTime = System.nanoTime();
-			System.out.println("\nDuracion: " + (endTime - startTime) / 1e6 + " ms");
+            long startTime = System.nanoTime();
+            while (input.read(buffer) != -1) {
+                for (int i = 0; i < buffer.length; i++)
+                    System.out.println("bloque " + i + " > byte = " + buffer[i]);
+            }
+            long endTime = System.nanoTime();
+            System.out.println("\nDuracion: " + (endTime - startTime) / 1e6 + " ms");
 
-		} catch (FileNotFoundException e) {
-			System.err.println("El archivo no existe!\nMas informacion...");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.err.println("Error de I/O!\nMas informaion...");
-			e.printStackTrace();
-		} finally {
-			try {
-				if (input != null) input.close();
-			} catch (IOException e) {
-				System.err.println("No se pudo cerrar el flujo de entrada!\nMas informacion...");
-				e.printStackTrace();
-			}
-		}
+        } catch (FileNotFoundException e) {
+            System.err.println("El archivo no existe!\nMas informacion...");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Error de I/O!\nMas informaion...");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (input != null) input.close();
+            } catch (IOException e) {
+                System.err.println("No se pudo cerrar el flujo de entrada!\nMas informacion...");
+                e.printStackTrace();
+            }
+        }
 
-	}
+    }
 
-	/**
-	 * Crea un flujo de salida hacia un nuevo archivo de imagen utilizando los bytes recibidos por parametro.
-	 * Es decir, que copia y pega una imagen en una ruta especificada.
-	 * 
-	 * @param bytes[] - Los bytes leidos de la imagen a copiar.
-	 */
-	private void writeTexture(byte[] bytes) {
-		try {
+    /**
+     * Crea un flujo de salida hacia un nuevo archivo de imagen utilizando los bytes recibidos por parametro.
+     * Es decir, que copia y pega una imagen en una ruta especificada.
+     *
+     * @param bytes - Los bytes leidos de la imagen a copiar.
+     */
+    private void writeTexture(byte[] bytes) {
+        try {
 
-			// Crea un archivo llamado "dragon_copia.png"
-			output = new FileOutputStream(new File(getTextureDir() + "dragon_copia.png"));
+            // Crea un archivo llamado "dragon_copia.png"
+            output = new FileOutputStream(new File(getTextureDir() + "dragon_copia.png"));
 
-			output.write(bytes);
+            output.write(bytes);
 
-		} catch (FileNotFoundException e) {
-			System.err.println("El archivo no existe!\nMas informacion...");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.err.println("Error de I/O!\nMas informacion...");
-			e.printStackTrace();
-		} finally {
-			try {
-				if (output != null) output.close();
-			} catch (IOException e) {
-				System.err.println("No se pudo cerrar el flujo de salida!\nMas informacion...");
-				e.printStackTrace();
-			}
-		}
-	}
+        } catch (FileNotFoundException e) {
+            System.err.println("El archivo no existe!\nMas informacion...");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Error de I/O!\nMas informacion...");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (output != null) output.close();
+            } catch (IOException e) {
+                System.err.println("No se pudo cerrar el flujo de salida!\nMas informacion...");
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ByteStream texture = new ByteStream(new File(BOLA_AMARILLA2));
-		texture.readTexture();
-		// texture.writeTexture(bytes);
+        ByteStream texture = new ByteStream(new File(BOLA_AMARILLA2));
+        texture.readTexture();
+        // texture.writeTexture(bytes);
 
-		ByteStream text = new ByteStream(new File(TEXT));
-		// text.readText();
-		// text.writeText("Tostado", false);
+        ByteStream text = new ByteStream(new File(TEXT));
+        // text.readText();
+        // text.writeText("Tostado", false);
 
-	}
+    }
 
 }
